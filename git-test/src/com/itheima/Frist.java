05b7eedeea0d202e10f5fd60423ae3b0e00838e8
xukunzhang <37403535+yangyuezhang@users.noreply.github.com>
2019-04-17 19:13:47 +0800

    本地工程添加到git

---
+++ git-test/src/com/itheima/Frist.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class Frist {
+    public static void main(String[] args) {
+        System.out.println("hello......");
+        System.out.println("hello......");
+        System.out.println("hello......");
+        System.out.println("hello......");
+        System.out.println("hello......");
+        System.out.println("hello......");
+    }
+}
